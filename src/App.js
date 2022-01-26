cf5975569b90479b39089405cf071be47d263947
Paul Jeon <49374104+pauljeonn@users.noreply.github.com>
2022-01-27 08:26:45 +0900

    Feat: UI 생성

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
 import './App.css';
+import Counter from './components/Counter';
+import Todos from './components/Todos';
 
 function App() {
-	return <div className="App">REDUX PRACTICE</div>;
+	return (
+		<div className="App">
+			<h1>REDUX PRACTICE</h1>
+			<Counter number={0} />
+			<hr />
+			<Todos />
+		</div>
+	);
 }
 
 export default App;
